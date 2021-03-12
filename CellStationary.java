249a1e9dbe58c34ed70b9b4f501110bc55598a08
danielnguyen446 <77251383+danielnguyen446@users.noreply.github.com>
2021-03-11 17:08:12 -0800

    Create CellStationary.java

---
+++ CellStationary.java
@@ -0,0 +1,80 @@
+/** 
+ * Name: Daniel Nguyen
+ * ID: A16129027
+ * Email: d7nguyen@ucsd.edu
+ * Sources used: tutor help(Stephen M Boussarov, Joona Kim)
+ * 
+ * This file contains the CellStationary subclass of cell. This
+ * concrete subclass is denoted by "." on the petri dish.
+ */
+
+import java.util.*;
+
+/**
+ * This class derives from cell. It has two constructors and overrides the 
+ * toString and checkApoptosis methods. 
+ */
+public class CellStationary extends Cell
+{
+    
+    /**
+     * This constructor will call the parent class' constructor to initialize
+     * the instance variables
+     * 
+     * @param currRow, currCol, mass
+     */
+    public CellStationary(int currRow, int currCol, int mass)
+    {
+        super(currRow, currCol, mass);
+    }
+    
+    /**
+     * This copy constructor will call the parent class' copy constructor
+     * to initialize the instance variables
+     * 
+     * @param otherCellStationary
+     */
+    public CellStationary(CellStationary otherCellStationary)
+    {
+        super(otherCellStationary);
+    }
+    
+    /**
+     * This method will return the string representation of the current object.
+     * 
+     * @return .
+     */
+    public String toString()
+    {
+        return ".";
+    }
+    
+    /**
+     * This method checks if the cell should initiate apoptosis or not by 
+     * determining the number of neighbors the cell has.
+     * 
+     * @param Cell list neighbors
+     * @return whether the conditions are met
+     */
+    public boolean checkApoptosis(List<Cell> neighbors)
+    {
+        if(neighbors.size()<=7 && neighbors.size()>=3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    /**
+     * This method will return a deep copy of the calling object.
+     * 
+     * @return Cell type
+     */
+    public Cell newCellCopy()
+    {
+        return new CellStationary(this);
+    }
+}
